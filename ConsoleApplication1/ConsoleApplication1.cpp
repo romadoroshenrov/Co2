10d3d8f8542d717fb7ff57713029abde06ab06d7
1_dal <faberliccca@gmail.com>
2023-09-20 01:49:00 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	const double PI = 3.141592;
+	cout << "Введите площадь:";
+	double a, b, s;
+	cin >> s;
+	a = s / PI;
+	b = pow(a, 0.5);
+	cout << "диаметр = :" << b * 2 << endl << "длинна окружности:" << b * PI * 2;
+}
